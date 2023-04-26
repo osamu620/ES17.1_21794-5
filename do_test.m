8a1c2e7c0f78940f07ca3a7b7923f073a1a80379
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-04-26 13:00:23 +0900

    Add tests for multi-threading

--- do_test.m
+++ do_test.m
@@ -2,9 +2,22 @@ if ~(exist('LOW', 'var') && exist('MEDIUM', 'var') && exist('HIGH', 'var'))
     prepare_test;
 end
 
-[nblow, tlow] = do_ec_htj2k("low", LOW, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin");
-[nbmed, tmed] = do_ec_htj2k("medium", MEDIUM, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin");
-[nbhigh, thigh] = do_ec_htj2k("high", HIGH, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin");
+N = 10;
+for i = 1:N
+    [nblow, tlow] = do_ec_htj2k("low", LOW, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin");
+    [nbmed, tmed] = do_ec_htj2k("medium", MEDIUM, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin");
+    [nbhigh, thigh] = do_ec_htj2k("high", HIGH, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin");
 
-csize = [nblow, nbmed, nbhigh];
-t = [tlow, tmed, thigh];
+    csize_s = [nblow, nbmed, nbhigh];
+    t_s(i, :) = [tlow, tmed, thigh];
+end
+
+for i = 1:N
+    [nblow, tlow] = do_ec_htj2k("low", LOW, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin", true);
+    [nbmed, tmed] = do_ec_htj2k("medium", MEDIUM, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin", true);
+    [nbhigh, thigh] = do_ec_htj2k("high", HIGH, "~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin", true);
+
+    csize_m = [nblow, nbmed, nbhigh];
+    t_m(i, :) = [tlow, tmed, thigh];
+end
+
